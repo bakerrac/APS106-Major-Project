4a04050bb80bfc6a47800f599a0c9e4b8e8ad8ad
bakerrac <rachel.baker@mail.utoronto.ca>
2013-03-04 17:40:02 -0500

    Update MainProject.c

--- MainProject.c
+++ MainProject.c
@@ -2,3 +2,18 @@
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
+#defult ROWS=10
+#defult COLS=15
+#defult NUMCOLORS=4
+
+void main(){
+  srand((unsigned)time(NULL));
+  char arrange[ROWS][COLS];
+  int i,j;
+  for(i=0;i<ROWS;i++){
+    for(j=0;j<COLS; j<){
+      arrange[i][j]=rand()%NUMCOLORS+'A';
+    }
+  }
+  return;
+}
